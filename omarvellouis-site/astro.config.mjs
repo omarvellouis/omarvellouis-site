5c65561c8e22a40b56f26c1198ea116542cea645
omarvellouis <omarvellouis@gmail.com>
2026-03-23 11:26:04 +0700

    Delete omarvellouis-site directory

--- omarvellouis-site/astro.config.mjs
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'astro/config';
-
-export default defineConfig({
-  site: 'https://omarvellouis.com',
-  trailingSlash: 'never',
-  build: {
-    inlineStylesheets: 'auto'
-  }
-});
